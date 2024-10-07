407d6e3e0a61e25b745edc5c3f821f4737ce7035
adellacae <137608697+adellacae@users.noreply.github.com>
2024-10-07 19:17:04 +0700

    Update test.js

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ describe('Pengujian Fungsi Matematika', function() {
  it('seharusnya mengembalikan 6 saat mengalikan 2 * 3', function() {
  expect(kali(2, 3)).to.equal(6);
  });
- it('seharusnya mengembalikan 4 saat menambahkan (-2) * (-2)', function() {
+ it('seharusnya mengembalikan 4 saat mengalikan (-2) * (-2)', function() {
     expect( kali ((-2), (-2))).to.equal(4);
     });
  it('seharusnya mengembalikan 0 saat mengurangkan 2 - 2', function() {
