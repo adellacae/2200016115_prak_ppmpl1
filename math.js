939cde035601a7d64c71f48c7e35e24bf358da86
adellacae <adellacaesardiniartika@gmail.com>
2024-10-07 18:49:31 +0700

    first commit

---
+++ math.js
@@ -0,0 +1,32 @@
+function tambah(a, b)
+{ if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Input tidak valid');
+}
+return a + b;
+} 
+function kali(a, b)
+{   if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Input tidak valid');
+}
+
+return a * b;
+}
+function kurang(a, b)
+{  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Input tidak valid');
+}
+
+    return a - b;
+} 
+function bagi(a, b) 
+{if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Input tidak valid');
+}
+
+    if (b === 0){
+    throw new Error('Tidak bisa membagi dengan nol');
+}
+return a / b;
+}
+
+module.exports = { tambah, kali, kurang, bagi };
---
+++ test.js
@@ -0,0 +1,41 @@
+const { expect } = require('chai');
+const { tambah, kali, kurang, bagi } = require('./math');
+describe('Pengujian Fungsi Matematika', function() {
+ it('seharusnya mengembalikan 4 saat menambahkan 2 + 2', function() {
+ expect(tambah(2, 2)).to.equal(4);
+ });
+ it('seharusnya mengembalikan -4 saat menambahkan (-2) + (-2)', function() {
+    expect( tambah ((-2), (-2))).to.equal(-4);
+    });
+ it('seharusnya mengembalikan 6 saat mengalikan 2 * 3', function() {
+ expect(kali(2, 3)).to.equal(6);
+ });
+ it('seharusnya mengembalikan 4 saat menambahkan (-2) * (-2)', function() {
+    expect( kali ((-2), (-2))).to.equal(4);
+    });
+ it('seharusnya mengembalikan 0 saat mengurangkan 2 - 2', function() {
+ expect(kurang(2, 2)).to.equal(0);
+ });
+ it('seharusnya mengembalikan -2 saat mengurangkan (-2) - (-2)', function() {
+    expect(kurang(-4, -2)).to.equal(-2);
+    });
+ it('seharusnya mengembalikan 2 saat membagi 6 / 3', function() {
+ expect(bagi(6, 3)).to.equal(2);
+ });
+ it('seharusnya mengembalikan -2 saat membagi -6 / 3', function() {
+    expect(bagi(-6, 3)).to.equal(-2);
+    });
+ it('seharusnya mengembalikan error saat membagi dengan 0', function() {
+ expect(() => bagi(6, 0)).to.throw('Tidak bisa membagi dengan nol');
+ });
+ it('seharusnya mengembalikan error saat input string ke fungsi tambah', function() {
+    expect(() => tambah('2', 2)).to.throw('Input tidak valid');
+    expect(() => tambah(2, '2')).to.throw('Input tidak valid');
+});
+
+it('seharusnya mengembalikan error saat input null ke fungsi tambah', function() {
+    expect(() => tambah(null, 2)).to.throw('Input tidak valid');
+    expect(() => tambah(2, null)).to.throw('Input tidak valid');
+});
+
+});
